0f33bb61e310f1b44beca2a1de119f6599cde671
lukasXOR <50531317+lukasXOR@users.noreply.github.com>
2023-02-09 11:45:16 +0000

    Add files via upload

---
+++ App01/DistanceConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleAppProject.App01
+{
+    /// <summary>
+    /// Please describe the main features of this App
+    /// </summary>
+    /// <author>
+    /// lukas
+    /// </author>
+    public class DistanceConverter
+    {
+        public static string createMenu(string[] msg)
+        {
+            for (int i = 0; i < msg.Length; i++)
+            {
+                Console.WriteLine(msg[i] + " " + (i + 1));
+            }
+            return Console.ReadLine();
+        }
+        public static string convertUnit(string from, string to, double input)
+        {
+            double userInput = input;
+            double newValue = 0;
+            //constants
+            int mileFeet = 5280;
+            double mileMetres = 1609.34;
+            double feetMetres = 0.3048;
+            switch (from)
+            {
+                case "miles":
+                    if (to == "feet") newValue = userInput * mileFeet;
+                    else if (to == "metres")  newValue = userInput * mileMetres;
+                    break;
+                case "feet":
+                    if (to == "miles") newValue = userInput / mileFeet;
+                    else if (to == "metres") newValue = userInput / feetMetres;
+                    break;
+                case "metres":
+                    if (to == "feet")  newValue = userInput * feetMetres;
+                    else if (to == "miles") newValue = userInput / mileMetres;
+                    break;
+            }
+            Console.WriteLine(from + " " + input + " is " + newValue + " in " + to);
+            return "";
+        }
+        public void run()
+        {
+            string[] menuOptions = {"miles", "feet", "metres" };
+
+            Console.WriteLine("unit to convert from?");
+            int optionFrom = int.Parse(createMenu(menuOptions)) - 1;
+
+            Console.WriteLine("unit to convert to?");
+            int optionTo = int.Parse(createMenu(menuOptions)) - 1;
+
+            Console.WriteLine("enter " + menuOptions[optionFrom]);
+            double input = Double.Parse(Console.ReadLine());
+
+            convertUnit(menuOptions[optionFrom], menuOptions[optionTo], input);
+
+        }
+    }
+}
---
+++ App02/BMI.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+namespace ConsoleAppProject.App02
+{
+    /// <summary>
+    /// Please describe the main features of this App
+    /// </summary>
+    /// <author>
+    /// Student Name version 0.1
+    /// </author>
+    public class BMI
+    {
+        public void run()
+        {
+            Console.WriteLine("enter weight in kg");
+            double usrWeight = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("height in metres");
+            double usrHeight = Math.Pow(Double.Parse(Console.ReadLine()), 2);
+
+            double usrBMI = usrWeight / usrHeight;
+            Console.Write(Math.Round(usrBMI, 4) + " ");
+
+            if (usrBMI < 18.5)
+                Console.WriteLine("underweight");
+            else if (usrBMI > 18.5 && usrBMI < 24.9)
+                Console.WriteLine("normal");
+            else if (usrBMI > 25 && usrBMI < 29.9)
+                Console.WriteLine("overweight");
+            else if (usrBMI > 30 && usrBMI < 34.9)
+                Console.WriteLine("obese class I");
+            else if (usrBMI > 35.0 && usrBMI < 39.9)
+                Console.WriteLine("obese class II");
+            else if (usrBMI > 40.0)
+                Console.WriteLine("obese class III");
+        }
+    }
+}
