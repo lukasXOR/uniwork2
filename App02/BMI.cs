192c1b3c8d388d5d8688995e4454bbfe4f7b9e85
lukas <lukas7865@yahoo.co.uk>
2023-02-13 18:54:32 +0000

    clean bmi

--- App02/BMI.cs
+++ App02/BMI.cs
@@ -8,8 +8,7 @@ namespace ConsoleAppProject.App02
     /// <author>
     /// Student Name version 0.1
     /// </author>
-    public class BMI
-    {
+    public class BMI {
         public void run() {
             Console.WriteLine("BMI Calculator");
             string[] menuOptions = {"imperial stones/pounds feet/inches", "metric kg metres"};
@@ -18,31 +17,26 @@ namespace ConsoleAppProject.App02
             double optionWeight = Utility.CreateOption("enter weight " + info[1], "input", new string[0]);
             Console.WriteLine();
             double optionHeight = Utility.CreateOption("enter height " + info[2], "input", new string[0]);
-            Utility.CleanConsole();
-            double userBMI = 0;
-            switch (optionUnit) {
-                case 1:
-                    userBMI = (optionWeight * 703) / Math.Pow(optionHeight, 2);
-                    break;
-                case 2:
-                    userBMI = optionWeight / Math.Pow(optionHeight, 2);
-                    break;
-            }
+            Console.WriteLine();
+            calcBMI((int)optionUnit, optionWeight, optionHeight);
+        }
+        public static void calcBMI(int unit, double weight, double height) {
+            double userBMI = (unit == 0 ? weight * 703 : weight) / Math.Pow(height, 2);
 
-            Console.Write("BMI" + Math.Round(userBMI, 4) + " ");
+            Console.Write("BMI " + Math.Round(userBMI, 4) + " ");
 
-            if (userBMI < 18.5)
-                Console.WriteLine(Unit.Underweight);
-            else if (userBMI > 18.5 && userBMI < 24.9)
-                Console.WriteLine(Unit.Normal);
-            else if (userBMI > 25 && userBMI < 29.9)
-                Console.WriteLine(Unit.Overweight);
-            else if (userBMI > 30 && userBMI < 34.9)
-                Console.WriteLine(Unit.ObeseI);
-            else if (userBMI > 35.0 && userBMI < 39.9)
-                Console.WriteLine(Unit.ObeseII);
-            else if (userBMI > 40.0)
-                Console.WriteLine(Unit.ObeseIII);
+            if (userBMI < 18.5) Console.WriteLine(Unit.Underweight);
+            else if (userBMI > 18.5 && userBMI < 24.9)  Console.WriteLine(Unit.Normal);
+            else if (userBMI > 25 && userBMI < 29.9) Console.WriteLine(Unit.Overweight);
+            else if (userBMI > 30 && userBMI < 34.9) Console.WriteLine(Unit.ObeseI);
+            else if (userBMI > 35.0 && userBMI < 39.9) Console.WriteLine(Unit.ObeseII);
+            else if (userBMI > 40.0) Console.WriteLine(Unit.ObeseIII);   
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("note that you should assume these values are inaccurate if");
+            Console.WriteLine("you are within minority ethnic groups (BAME) or that you are a");
+            Console.WriteLine("child, pregnant or someone with a signigicant amount of muscle");
+            Console.ForegroundColor = ConsoleColor.Cyan;
         }
     }
 }
