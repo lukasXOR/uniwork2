e5026f0fac8dc3e7d07140ad41e850d5c8a190c0
lukasXOR <50531317+lukasXOR@users.noreply.github.com>
2023-03-16 23:08:19 +0000

    add analyse page

--- webapp/wwwroot/js/site.js
+++ webapp/wwwroot/js/site.js
@@ -2,9 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-let studentDBJSON = {}
 
-let addStudentBtn = document.getElementById("addStudentBtn")
+let studentDBJSON = {}
+let addStudentBtn = document.getElementById("addStudentBtn") || {}
 let studentSection = document.getElementById("studentSection")
 
 localStorage.getItem("students") || localStorage.setItem("students", JSON.stringify(studentDBJSON))
@@ -16,14 +16,13 @@ addStudentBtn.onclick = function () {
     let studentName = document.getElementById("studentName").value
     let studentMark = document.getElementById("studentMark").value
 
-    studentDBJSON = { ...studentDBJSON, [studentName] : studentMark}
+    studentDBJSON = { ...studentDBJSON, [studentName] : Number(studentMark)}
     localStorage.setItem("students", JSON.stringify(studentDBJSON))
 
     updateDisplay()
 
     document.getElementById("studentName").value = ''
     document.getElementById("studentMark").value = ''
-
 }
 function updateDisplay() {
     studentSection.innerHTML = ''
@@ -40,12 +39,113 @@ function getStudentHTML(name, mark, length) {
     <ol class="list-group">
         <li class="list-group-item">
             <a id="studentName${length}">Name: ${name}</a>
-            <button type="button" id="studentName${length}Edit">Edit</button>
         </li>
         <li class="list-group-item">
             <a id="studentMark${length}">Mark: ${mark}</a>
-            <button type="button" id="studentMark${length}Edit">Edit</button>
         </li>
     </ol>
 `
+}
+class Students {
+    constructor(students) {
+        this.students = students
+        this.studentsLength = Object.keys(students).length
+    }
+    FindGrade(grade) {
+        if (grade <= 39) return 'F';
+        else if (grade >= 40 && grade < 50) return 'D';
+        else if (grade >= 50 && grade < 60) return 'C';
+        else if (grade >= 60 && grade < 70) return 'B';
+        else if (grade >= 75 && grade < 101) return 'A';
+
+        return 'A';
+    }
+    getGradeProfile() {
+        let studentLength = Object.keys(this.students).length
+        let grades = {
+            A: 0,
+            B: 0,
+            C: 0,
+            D: 0,
+            F: 0,
+        }
+        for (const student in this.students) {
+            switch (this.FindGrade(this.students[student])) {
+                case 'A':
+                    grades.A++;
+                    break;
+                case 'B':
+                    grades.B++;
+                    break;
+                case 'C':
+                    grades.C++;
+                    break;
+                case 'D':
+                    grades.D++;
+                    break;
+                case 'F':
+                    grades.F++;
+                    break;
+            }
+        }
+        for (const grade in grades) {
+            grades[grade] = Math.round((grades[grade] / studentLength) * 100)
+        }
+        return grades
+    }
+    get min() {
+        let minMark = 0
+        for (const student in this.students)
+            if (minMark > this.students[student] || minMark === 0)
+                minMark = this.students[student]
+        return minMark
+    }
+    get max() {
+        let maxMark = 0
+        for (const student in this.students)
+            if (maxMark < this.students[student])
+                maxMark = this.students[student]
+        return maxMark
+    }
+    get mean() {
+        let meanMark = 0
+        for (const student in this.students) {
+            meanMark += this.students[student]
+        }
+        return Math.round(meanMark / this.studentsLength)
+    }
+}
+function getStudentGradeHTML(name, grade) {
+    return `
+  <div class="grade">
+    ${name}: ${grade}%
+  </div>
+`
+}
+if (location.href.includes('studentmarksanalyse')) addStudentAnalytics()
+function addStudentAnalytics() {
+    let studentCookie = JSON.parse(localStorage.getItem('students'))
+    let studentGrades = new Students(studentCookie)
+    let studentAnalyseID = document.getElementById('studentAnalyse')
+    let gradeProfile = studentGrades.getGradeProfile()
+    let studentGradeArray = [['test','test']]
+    document.getElementById('numStudents').innerText = studentGrades.studentsLength
+    document.getElementById('minStudents').innerText = studentGrades.min
+    document.getElementById('maxStudents').innerText = studentGrades.max
+    document.getElementById('meanStudents').innerText = studentGrades.mean
+    for (grade in gradeProfile) {
+        studentAnalyseID.innerHTML += getStudentGradeHTML(grade, gradeProfile[grade])
+        studentGradeArray.push([grade, gradeProfile[grade]])
+    }
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(function () { drawChart(studentGradeArray) });
+}
+
+function drawChart(chartArray) {
+    var data = google.visualization.arrayToDataTable(chartArray);
+    var options = {
+        title: 'Student Grade Profile'
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
 }
