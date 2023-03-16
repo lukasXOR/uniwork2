cdf4c7f6e178594f26b2d121cc621d0c421dd748
lukasXOR <50531317+lukasXOR@users.noreply.github.com>
2023-03-16 23:07:19 +0000

    remove unnecessary lib

--- App03/StudentGrades.cs
+++ App03/StudentGrades.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Security.AccessControl;
-
 namespace ConsoleAppProject.App03
 {
     /// <summary>
