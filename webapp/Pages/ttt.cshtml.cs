1ace7b601cdd674e534bf8a8766ab1ee694c92e3
lukasXOR <50531317+lukasXOR@users.noreply.github.com>
2023-05-11 12:53:29 +0100

    app5 html

---
+++ webapp/Pages/ttt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages
+{
+    public class tttModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
